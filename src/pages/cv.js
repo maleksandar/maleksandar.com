0557a2459d78c849267ecdff80f700dd9bd45c1a
Aleksandar Milosavljević <alemilosav@gmail.com>
2020-07-15 22:54:57 +0200

    Cv - init component

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
+import Cv from '../components/Cv';
 import { useSiteMetadata } from '../hooks';
 import styles from './Cv.module.scss';
 
@@ -16,11 +17,8 @@ const IndexTemplate = () => {
       <div className={styles['cv']}>
           
           <div className={styles['cv__inner']}>
-            <h1 className={styles['cv__title']}>Sadrzaj CVa</h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <h1 className={styles['cv__title']}>CV</h1>
+            <Cv/>
           </div>
       </div>
     </Layout>
